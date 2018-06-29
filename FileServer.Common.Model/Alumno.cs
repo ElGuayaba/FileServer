c9899a4f5f411d142406a020dfa9165bfc948007
Jose Alamo <alamose@hotmail.com>
2018-06-29 17:45:54 +0200

    continuación de la estructura

--- FileServer.Common.Model/Alumno.cs
+++ FileServer.Common.Model/Alumno.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FileServer.Common.Model
 {
     public class Alumno
     {
-    }
+		public int Id { get; set; }
+		public string Nombre { get; set; }
+		public string Apellido { get; set; }
+		public string Dni { get; set; }
+		public Alumno(int Id, string Nombre, string Apellido, string Dni)
+		{
+			this.Id = Id;
+			this.Nombre = Nombre;
+			this.Apellido = Apellido;
+			this.Dni = Dni;
+		}
+
+		public override bool Equals(object obj)
+		{
+			var alumno = obj as Alumno;
+			return alumno != null &&
+				   Id == alumno.Id &&
+				   Nombre == alumno.Nombre &&
+				   Apellido == alumno.Apellido &&
+				   Dni == alumno.Dni;
+		}
+
+		public override int GetHashCode()
+		{
+			var hashCode = -1910929195;
+			hashCode = hashCode * -1521134295 + Id.GetHashCode();
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nombre);
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Apellido);
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Dni);
+			return hashCode;
+		}
+	}
 }
